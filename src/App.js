de7e6d65d63f244f362dab7b4091f9d7688d4731
Adrian <adriantutuianu01@gmail.com>
2021-11-18 23:30:49 +0000

    Installed AntDesign

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
+import "antd/dist/antd.css";
+import { DatePicker } from "antd";
 
 function App() {
   return (
     <div className="App">
       <p style={{ color: "var(--primary-color)" }}>Hello!! </p>
+      <DatePicker />
     </div>
   );
 }
