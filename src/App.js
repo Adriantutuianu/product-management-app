6bf86d9557b6e65d7420443b59e049439c5e5d80
Adrian <adriantutuianu01@gmail.com>
2021-11-21 14:05:26 +0000

    Header.js and header.css created

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import "./antd.css";
 import "./App.css";
 import { DatePicker } from "antd";
 import Footer from "./Footer";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
       <p style={{ color: "var(--primary-color)" }}>Hello!! </p>
       <DatePicker />
-
+      <Header />
       <Footer />
     </div>
   );
