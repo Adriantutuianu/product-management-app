0472de334ec08505e7cb9e602cf2db0bad14a6a4
Adrian <adriantutuianu01@gmail.com>
2021-11-22 23:37:25 +0000

    Added menu

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,18 @@
 import React from "react";
+import { Menu } from "antd";
 import "./header.css";
 
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header>
+      <Menu defaultSelectedKeys={["1"]} style={{ display: "flex" }}>
+        <Menu.Item key="1">Products</Menu.Item>
+        <Menu.Item key="2">View Categories</Menu.Item>
+        <Menu.Item key="3">Manage Users</Menu.Item>
+        <Menu.Item key="4">About</Menu.Item>
+      </Menu>
+    </header>
+  );
 };
 
 export default Header;
