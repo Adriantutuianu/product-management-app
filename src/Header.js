3e71a3143a24b8ebb221046500d0363c98e1e5c4
Adrian <adriantutuianu01@gmail.com>
2021-11-23 11:46:16 +0000

    Test image

--- src/Header.js
+++ src/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
         defaultSelectedKeys={["1"]}
         style={{ display: "flex" }}
       >
-        <img src={process.env.PUBLIC_URL + "/logo192.png"} />
+        <img src={process.env.PUBLIC_URL + "/logo192.png"} alt="Product logo" />
         <Menu.Item key="1">Products</Menu.Item>
         <Menu.Item key="2">View Categories</Menu.Item>
         <Menu.Item key="3">Manage Users</Menu.Item>
