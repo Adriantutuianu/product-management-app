2cf6e334485e28b9ddb53972ff3b8453764635ec
Adrian <adriantutuianu01@gmail.com>
2021-11-23 12:22:47 +0000

    Header display

--- src/Header.js
+++ src/Header.js
@@ -5,12 +5,12 @@ import "./header.css";
 const Header = () => {
   return (
     <header>
+      <img src={process.env.PUBLIC_URL + "/logo192.png"} alt="Product logo" />
       <Menu
         className="header-menu"
         defaultSelectedKeys={["1"]}
         style={{ display: "flex" }}
       >
-        <img src={process.env.PUBLIC_URL + "/logo192.png"} alt="Product logo" />
         <Menu.Item key="1">Products</Menu.Item>
         <Menu.Item key="2">View Categories</Menu.Item>
         <Menu.Item key="3">Manage Users</Menu.Item>
