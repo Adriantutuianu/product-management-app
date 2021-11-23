e5f61b7804b485de8cb5b5f5f97c5efebfec8588
Adrian <adriantutuianu01@gmail.com>
2021-11-23 12:36:55 +0000

    Styled header

--- src/Header.js
+++ src/Header.js
@@ -6,17 +6,19 @@ import Auth from "./Auth.js";
 const Header = () => {
   return (
     <header>
-      <img src={process.env.PUBLIC_URL + "/logo192.png"} alt="Product logo" />
-      <Menu
-        className="header-menu"
-        defaultSelectedKeys={["1"]}
-        style={{ display: "flex" }}
-      >
-        <Menu.Item key="1">Products</Menu.Item>
-        <Menu.Item key="2">View Categories</Menu.Item>
-        <Menu.Item key="3">Manage Users</Menu.Item>
-        <Menu.Item key="4">About</Menu.Item>
-      </Menu>
+      <section style={{ display: "flex" }}>
+        <img src={process.env.PUBLIC_URL + "/logo192.png"} alt="Product logo" />
+        <Menu
+          className="header-menu"
+          defaultSelectedKeys={["1"]}
+          style={{ display: "flex" }}
+        >
+          <Menu.Item key="1">Products</Menu.Item>
+          <Menu.Item key="2">View Categories</Menu.Item>
+          <Menu.Item key="3">Manage Users</Menu.Item>
+          <Menu.Item key="4">About</Menu.Item>
+        </Menu>
+      </section>
       <Auth />
     </header>
   );
