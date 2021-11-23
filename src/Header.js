7e51cd87754b9f3a4493b88df8c02b8cbcec6bf9
Adrian <adriantutuianu01@gmail.com>
2021-11-23 11:28:13 +0000

    Image added from public- header

--- src/Header.js
+++ src/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
         defaultSelectedKeys={["1"]}
         style={{ display: "flex" }}
       >
-        <img src="/logo192.png" alt="image" />
+        <img src={process.env.PUBLIC_URL + "/logo192.png"} />
         <Menu.Item key="1">Products</Menu.Item>
         <Menu.Item key="2">View Categories</Menu.Item>
         <Menu.Item key="3">Manage Users</Menu.Item>
