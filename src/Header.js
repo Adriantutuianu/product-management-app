e5c745ab17f427954e00d2e0f2c481a29ed6ffac
Adrian <adriantutuianu01@gmail.com>
2021-11-23 12:54:43 +0000

    Finished header and menu

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Menu } from "antd";
 import "./header.css";
-import Auth from "./Auth.js";
+import Auth from "./auth/Auth.js";
 
 const Header = () => {
   return (
---
+++ src/auth/Auth.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./auth.css";
+import { Button } from "antd";
+
+const Auth = () => {
+  return (
+    <div className="auth">
+      <Button type="primary">Login</Button>;
+    </div>
+  );
+};
+
+export default Auth;
