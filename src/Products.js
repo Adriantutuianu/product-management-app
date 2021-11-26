4159251f103ccd430dc5f4214b3bdd2764c1aec5
Adrian <adriantutuianu01@gmail.com>
2021-11-26 23:12:53 +0000

    Created Products.js

---
+++ src/Products.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Products extends Component {
+  render() {
+    return (
+      <div>
+        <h2>HELLO</h2>
+        <p>!!!!</p>
+      </div>
+    );
+  }
+}
+
+export default Products;
