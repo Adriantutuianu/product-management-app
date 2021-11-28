597b4d545d0eff02adfa6ba3bdd2fa1796420d7f
Adrian <adriantutuianu01@gmail.com>
2021-11-28 20:58:53 +0000

    Added login modal

--- src/auth/Auth.js
+++ src/auth/Auth.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./auth.css";
-import { Button } from "antd";
+import { Modal, Button } from "antd";
 
 const Auth = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
   return (
     <div className="auth">
-      <Button type="primary">Login</Button>;
+      <Button type="primary" onClick={showModal}>
+        Login
+      </Button>
+      ;
+      <Modal
+        title="Basic Modal"
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
     </div>
   );
 };
