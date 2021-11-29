0fcbbc1b90b308af2e03780de8f6f12041180ff5
Adrian <adriantutuianu01@gmail.com>
2021-11-29 22:29:02 +0000

    Styled modal

--- src/auth/Auth.js
+++ src/auth/Auth.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./auth.css";
-import { Modal, Button, Input, Space } from "antd";
+import { Modal, Button, Input } from "antd";
 import { EyeInvisibleOutlined, EyeTwoTone } from "@ant-design/icons";
 
 const Auth = () => {
@@ -25,12 +25,10 @@ const Auth = () => {
   };
 
   const onChangeUsername = (e) => {
-    console.log("Change:", e.target.value);
     setUsernameInputValue(e.target.value);
   };
 
   const onChangePassword = (e) => {
-    console.log("Change:", e.target.value);
     setPasswordInputValue(e.target.value);
   };
 
@@ -45,23 +43,22 @@ const Auth = () => {
         visible={isModalVisible}
         onOk={handleOk}
         onCancel={handleCancel}
+        style={{ maxWidth: "300px" }}
       >
         <Input
           placeholder="Username"
           onChange={onChangeUsername}
           value={usernameInputValue}
+          style={{ marginBottom: "8px" }}
         />
-        <Space direction="vertical">
-          <Input.Password
-            placeholder="input password"
-            iconRender={(visible) =>
-              visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
-            }
-            onChange={onChangePassword}
-            value={passwordInputValue}
-          />
-        </Space>
-        ,
+        <Input.Password
+          placeholder="input password"
+          iconRender={(visible) =>
+            visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+          }
+          onChange={onChangePassword}
+          value={passwordInputValue}
+        />
       </Modal>
     </div>
   );
