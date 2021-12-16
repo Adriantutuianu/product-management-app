cb1de3e42d6e8f48574abd7f38720a3fad1cbf28
Adrian <adriantutuianu01@gmail.com>
2021-12-17 01:33:06 +0200

    Title added -about page

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { Typography, Divider } from "antd";
+
+const { Title, Paragraph, Text, Link } = Typography;
 
 const About = () => {
-  return <div>About content</div>;
+  return <Title level={2}>Introduction</Title>;
 };
 
 export default About;
