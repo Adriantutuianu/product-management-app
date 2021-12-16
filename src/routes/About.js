23206a46dcf9bb5f24554b2c9f33fb7437e91b8b
Adrian <adriantutuianu01@gmail.com>
2021-12-17 01:54:02 +0200

    Added  Paragraph italic -about page

--- src/routes/About.js
+++ src/routes/About.js
@@ -20,6 +20,17 @@ const About = () => {
           record does not exist, the create operation adds it to the database.
         </Text>
       </Paragraph>
+      <Paragraph>
+        <Text italic>
+          Read is the operation that allows us to see the recipe we just
+          created. It does not alter data. It simply displays it. Read consists
+          of a function that queries our database and fetches all our records,
+          in this case, recipes. We can tailor our queries to pull back all
+          recipes, or only recipes of a particular type (for example,, vegan
+          recipes). We can also pull back a single recipe if we have a UID
+          (unique identifier).
+        </Text>
+      </Paragraph>
       <Text mark>
         This page is build using a fake API and it's not going to make any
         change in the database.
