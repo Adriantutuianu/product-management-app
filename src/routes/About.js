e9d0ffdbb45ca742fc6d292821f602caddf7d9d1
Adrian <adriantutuianu01@gmail.com>
2021-12-17 01:39:56 +0200

    Added text Mark - about page

--- src/routes/About.js
+++ src/routes/About.js
@@ -4,7 +4,15 @@ import { Typography, Divider } from "antd";
 const { Title, Paragraph, Text, Link } = Typography;
 
 const About = () => {
-  return <Title level={2}>Introduction</Title>;
+  return (
+    <div>
+      <Title level={2}>Introduction</Title>
+      <Text mark>
+        This page is build using a fake API and it's not going to make any
+        change in the database.
+      </Text>
+    </div>
+  );
 };
 
 export default About;
