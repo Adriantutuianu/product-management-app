ef5c94d4221506f11641e8cfa4d94c4e57f4c107
Adrian <adriantutuianu01@gmail.com>
2021-12-26 22:38:30 +0200

    Added list in Categories- example

--- src/routes/Categories.js
+++ src/routes/Categories.js
@@ -1,8 +1,27 @@
 import React from "react";
 import TitleComp from "../components/Title";
+import { List } from "antd";
+
+const data = [
+  "Racing car sprays burning fuel into crowd.",
+  "Japanese princess to wed commoner.",
+  "Australian walks 100km after outback crash.",
+  "Man charged over missing wedding girl.",
+  "Los Angeles battles huge wildfires.",
+];
 
 const Categories = () => {
-  return <TitleComp title="Categories" />;
+  return (
+    <>
+      <TitleComp title="Categories" />
+      <List
+        size="large"
+        bordered
+        dataSource={data}
+        renderItem={(item) => <List.Item>{item}</List.Item>}
+      />
+    </>
+  );
 };
 
 export default Categories;
