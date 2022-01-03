6b04efb2e1d4cea7fb160085ec00c68e8263c7a4
Adrian <adriantutuianu01@gmail.com>
2022-01-03 22:59:39 +0200

    Displayed categories

--- src/routes/Categories.js
+++ src/routes/Categories.js
@@ -2,14 +2,6 @@ import React, { useState, useEffect } from "react";
 import TitleComp from "../components/Title";
 import { List } from "antd";
 
-const data = [
-  "Racing car sprays burning fuel into crowd.",
-  "Japanese princess to wed commoner.",
-  "Australian walks 100km after outback crash.",
-  "Man charged over missing wedding girl.",
-  "Los Angeles battles huge wildfires.",
-];
-
 const Categories = () => {
   const [categories, setCategories] = useState([]);
 
@@ -32,14 +24,13 @@ const Categories = () => {
     getCategories(categoriesEndpoint);
   }, []);
 
-  console.log(categories);
   return (
     <>
       <TitleComp title="Categories" />
       <List
         size="large"
         bordered
-        dataSource={data}
+        dataSource={categories}
         renderItem={(item) => <List.Item>{item}</List.Item>}
       />
     </>
