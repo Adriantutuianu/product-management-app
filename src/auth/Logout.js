ecd45b63f0caf0546f7b32f2f7a1b79f9d4152ff
Adrian <adriantutuianu01@gmail.com>
2022-01-22 23:08:13 +0000

    Notification added

--- src/auth/Logout.js
+++ src/auth/Logout.js
@@ -1,9 +1,13 @@
 import React from "react";
-import { Button } from "antd";
+import { Button, notification } from "antd";
 
 const Logout = ({ setToken }) => {
   const handleClickLogout = () => {
     setToken("");
+    notification.open({
+      message: `Authentication`,
+      description: "You have been logged out!",
+    });
     console.log("You have been logged out!");
   };
 
