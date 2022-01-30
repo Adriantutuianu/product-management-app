d32c29c8b802788400071dad9cf7b1bdadf44120
Adrian <adriantutuianu01@gmail.com>
2022-01-30 22:22:36 +0000

    Added table -users

--- src/routes/Users.js
+++ src/routes/Users.js
@@ -1,8 +1,31 @@
 import React from "react";
 import TitleComp from "../components/Title";
+import { Table } from "antd";
 
 const Users = () => {
-  return <TitleComp title="Users" />;
+  const columns = [
+    {
+      title: "Id",
+      dataIndex: "id",
+      key: "id",
+    },
+    {
+      title: "User",
+      dataIndex: "user",
+      key: "user",
+    },
+  ];
+
+  return (
+    <>
+      <TitleComp title="Users" />
+      <Table
+        className="users-table"
+        columns={columns}
+        pagination={{ pageSize: 10 }}
+      />
+    </>
+  );
 };
 
 export default Users;
