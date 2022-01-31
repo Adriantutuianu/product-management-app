b34b6cb768a88734e616b3f9181ced0b12305e9f
Adrian <adriantutuianu01@gmail.com>
2022-01-31 22:51:24 +0000

    Conditional rendering of menu items

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ import Auth from "../auth/Auth.js";
 import { Link } from "react-router-dom";
 
 const Header = ({ credentials }) => {
+  console.log(credentials.token);
   return (
     <header>
       <section style={{ display: "flex" }}>
@@ -15,24 +16,27 @@ const Header = ({ credentials }) => {
             alt="Product logo"
           />
         </Link>
-        <Menu
-          className="header-menu"
-          defaultSelectedKeys={["1"]}
-          style={{ display: "flex" }}
-        >
-          <Menu.Item key="1">
-            <Link to="/">Products</Link>
-          </Menu.Item>
-          <Menu.Item key="2">
-            <Link to="/categories">View Categories</Link>
-          </Menu.Item>
-          <Menu.Item key="3">
-            <Link to="users">Manage Users</Link>
-          </Menu.Item>
-          <Menu.Item key="4">
-            <Link to="about">About</Link>
-          </Menu.Item>
-        </Menu>
+
+        {credentials.token && (
+          <Menu
+            className="header-menu"
+            defaultSelectedKeys={["1"]}
+            style={{ display: "flex" }}
+          >
+            <Menu.Item key="1">
+              <Link to="/">Products</Link>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Link to="/categories">View Categories</Link>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <Link to="users">Manage Users</Link>
+            </Menu.Item>
+            <Menu.Item key="4">
+              <Link to="about">About</Link>
+            </Menu.Item>
+          </Menu>
+        )}
       </section>
       <Auth credentials={credentials} />
     </header>
