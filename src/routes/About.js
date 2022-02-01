d6497f63c59184746ad4bc73bf1c00c283d5efc4
Adrian <adriantutuianu01@gmail.com>
2022-02-01 12:13:52 +0000

    Finished styling About page

--- src/routes/About.js
+++ src/routes/About.js
@@ -9,13 +9,11 @@ const About = () => {
   return (
     <div className="about">
       <TitleComp title="About" />
-
-      <ul>
-        {" "}
-        <Paragraph>
-          Project Management App is a SPA (Single Page Application) built in
-          React that will have the following functionalities:
-        </Paragraph>
+      <Paragraph>
+        Project Management App is a SPA (Single Page Application) built in React
+        that will have the following functionalities:
+      </Paragraph>
+      <ul style={{ marginLeft: "30px" }}>
         <li> Routing (React Router v6)</li>
         <li> Authentication</li>
         <li> Pagination</li>
@@ -29,21 +27,17 @@ const About = () => {
         application that consists of four basic operations; Create, Read,
         Update, Delete.
       </Paragraph>
-
       <Paragraph>
         The Product Management application was built using a fake API and it's
         not going to make any changes in the database.
       </Paragraph>
-
       <Paragraph>
         FakeStoreApi is a free online REST API that you can use whenever you
         need Pseudo-real data for your e-commerce or shopping website without
         running any server-side code. It's awesome for teaching purposes, sample
         codes, tests, etc.
       </Paragraph>
-
       <Paragraph>To build the app I have used Ant Design UI Kit.</Paragraph>
-
       <Paragraph>
         Also I have used Github Issues (as a Jira story) to organize and created
         separate branch for each feature.
