cb3fc4e1a5e8fdd6b7d1f20d07a3d522ba345d58
Adrian <adriantutuianu01@gmail.com>
2022-02-01 12:26:18 +0000

    Uodated text

--- src/components/NotLoggedIn.js
+++ src/components/NotLoggedIn.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Typography } from "antd";
+
+const { Text } = Typography;
 
 const NotLoggedIn = () => {
-  return <div>You are not logged in.</div>;
+  return (
+    <div>
+      <Text
+        strong
+        style={{ fontSize: "20px", display: "flex", alignItems: "center" }}
+      >
+        Please login first.
+      </Text>
+    </div>
+  );
 };
 
 export default NotLoggedIn;
