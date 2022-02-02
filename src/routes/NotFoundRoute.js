d68bcb2f56f324b8b220d1abba0b310b23b97170
Adrian <adriantutuianu01@gmail.com>
2022-02-02 22:41:04 +0000

    Finished styling not found route

--- src/routes/NotFoundRoute.js
+++ src/routes/NotFoundRoute.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Typography } from "antd";
+
+const { Text } = Typography;
 
 const NotFoundRoute = () => {
-  return <div>Page does not exist.</div>;
+  return (
+    <Text
+      strong
+      style={{
+        fontSize: "24px",
+        height: "calc(100vh - 120px)",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      Page does not exist.
+    </Text>
+  );
 };
 
 export default NotFoundRoute;
