0f6304d7f7979b1e034a50f575e57e2d7883d445
Adrian <adriantutuianu01@gmail.com>
2022-04-10 22:56:08 +0100

    Saved in state product details

--- src/routes/ProductDetails.js
+++ src/routes/ProductDetails.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 const ProductDetails = () => {
+  const [details, setDetails] = useState({});
+
   const { id } = useParams();
   console.log(id);
 
-  fetch(`https://fakestoreapi.com/products/${id}`)
-    .then((res) => res.json())
-    .then((json) => console.log(json));
+  useEffect(() => {
+    const getDetails = async () => {
+      try {
+        await fetch(`https://fakestoreapi.com/products/${id}`)
+          .then((res) => res.json())
+          .then((res) => setDetails(res));
+      } catch (error) {
+        console.log("Failed to get a single product: " + error);
+      }
+    };
+
+    getDetails();
+  }, []);
 
   return <div> {id}</div>;
 };
