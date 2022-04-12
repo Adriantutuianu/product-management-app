6e20a9d6fa8f945479b91b5054dd952b336419e2
Adrian <adriantutuianu01@gmail.com>
2022-04-12 23:50:45 +0100

    Update product details

--- src/routes/ProductDetails.js
+++ src/routes/ProductDetails.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Image, Rate, Descriptions } from "antd";
+import "./productsDetails.css";
 
 const ProductDetails = () => {
   const [details, setDetails] = useState({});
@@ -25,33 +26,30 @@ const ProductDetails = () => {
   return (
     <div className="product-details">
       {" "}
-      <Descriptions
-        title="Product details"
-        layout="vertical"
-        bordered
-      ></Descriptions>
-      <Descriptions.Item label="ID">Id: {details.id}</Descriptions.Item>
-      <br />
-      <Descriptions.Item label="Title">
-        Title: {details.title}
-      </Descriptions.Item>
-      <br />
-      <Descriptions.Item label="Description">
-        Description: {details.description}
-      </Descriptions.Item>
-      <br />
-      <Descriptions.Item label="Category">
-        Category: {details.category}
-      </Descriptions.Item>
-      <br />
-      <Descriptions.Item label="Image">
-        Image: <Image width={200} src={details.image} />
-      </Descriptions.Item>
-      <br />
-      {/* {details && details.rating && details.rating.rate} */}
-      <Descriptions.Item label="Rating">
-        Rating: <Rate value={details?.rating?.rate} disabled={true} />
-      </Descriptions.Item>
+      <Descriptions title="Product details" layout="vertical" bordered>
+        <Descriptions.Item label="ID">Id: {details.id}</Descriptions.Item>
+        {/* <br /> */}
+        <Descriptions.Item label="Title">
+          Title: {details.title}
+        </Descriptions.Item>
+        {/* <br /> */}
+        <Descriptions.Item label="Description">
+          Description: {details.description}
+        </Descriptions.Item>
+        {/* <br /> */}
+        <Descriptions.Item label="Category">
+          Category: {details.category}
+        </Descriptions.Item>
+        {/* <br /> */}
+        <Descriptions.Item label="Image">
+          <Image width={100} src={details.image} />
+        </Descriptions.Item>
+        {/* <br /> */}
+        {/* {details && details.rating && details.rating.rate} */}
+        <Descriptions.Item label="Rating">
+          Rating: <Rate value={details?.rating?.rate} disabled={true} />
+        </Descriptions.Item>
+      </Descriptions>
     </div>
   );
 };
